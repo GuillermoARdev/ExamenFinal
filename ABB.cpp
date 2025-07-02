1bdd89d930ae183fac74ebf5a2a8e49fe881d448
GuillermoARdev <75824639@continental.edu.pe>
2025-07-01 20:41:16 -0500

    in, post y preorden

--- ABB.cpp
+++ ABB.cpp
@@ -22,3 +22,35 @@ private:
         if (nodo == nullptr) {
             cout << "Elemento no existe, se acaba de ingresar." << endl;
             return new Nodo(valor);
+ if (valor < nodo->valor)
+            nodo->izquierdo = insertar(nodo->izquierdo, valor);
+        else if (valor > nodo->valor)
+            nodo->derecho = insertar(nodo->derecho, valor);
+        else
+            cout << "El valor ya existe en el árbol." << endl;
+        return nodo;
+    }
+
+    void inOrden(Nodo* nodo) {
+        if (nodo != nullptr) {
+            inOrden(nodo->izquierdo);
+            cout << nodo->valor << " ";
+            inOrden(nodo->derecho);
+        }
+    }
+
+    void preOrden(Nodo* nodo) {
+        if (nodo != nullptr) {
+            cout << nodo->valor << " ";
+            preOrden(nodo->izquierdo);
+            preOrden(nodo->derecho);
+        }
+    }
+
+    void postOrden(Nodo* nodo) {
+        if (nodo != nullptr) {
+            postOrden(nodo->izquierdo);
+            postOrden(nodo->derecho);
+            cout << nodo->valor << " ";
+        }
+    }
