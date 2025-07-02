f93e5da57f9ebd8c2008c3c10f0bb0f5cfb4d54b
GuillermoARdev <75824639@continental.edu.pe>
2025-07-01 20:44:50 -0500

    menu y recorridos

--- ABB.cpp
+++ ABB.cpp
@@ -27,7 +27,7 @@ private:
         else if (valor > nodo->valor)
             nodo->derecho = insertar(nodo->derecho, valor);
         else
-            cout << "El valor ya existe en el árbol." << endl;
+            cout << "El valor ya existe en el arbol." << endl;
         return nodo;
     }
 
@@ -53,4 +53,50 @@ private:
             postOrden(nodo->derecho);
             cout << nodo->valor << " ";
         }
+    }
+ }
+
+    void mostrarDecision(int valor) {
+        if (valor == 20) cout << "\tTe interesa el deporte\n";
+        else if (valor == 12) cout << "\tPractica DEPORTES DE EQUIPO\n";
+        else if (valor == 8) cout << "\tPractica FÚTBOL\n";
+        else if (valor == 15) cout << "\tPractica VOLEIBOL\n";
+        else if (valor == 30) cout << "\tPractica DEPORTES INDIVIDUALES\n";
+        else if (valor == 25) cout << "\tPractica NATACION\n";
+        else if (valor == 35) cout << "\tPractica TENIS\n";
+    }
+
+    bool buscar(Nodo* nodo, int valor) {
+        if (nodo == nullptr)
+            return false;
+        if (nodo->valor == valor) {
+            mostrarDecision(20); // Siempre mostrar primero el mensaje raiz
+            mostrarDecision(valor);
+            return true;
+        }
+        if (valor < nodo->valor)
+            return buscar(nodo->izquierdo, valor);
+        else
+            return buscar(nodo->derecho, valor);
+    }
+
+public:
+    ArbolBinario() {
+        raiz = nullptr;
+    }
+
+    void insertar(int valor) {
+        raiz = insertar(raiz, valor);
+    }
+
+    void mostrarInOrden() {
+        cout << "Recorrido inorden: ";
+        inOrden(raiz);
+        cout << endl;
+    }
+
+    void mostrarPreOrden() {
+        cout << "Recorrido preorden: ";
+        preOrden(raiz);
+        cout << endl;
     }
